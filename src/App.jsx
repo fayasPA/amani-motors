fdd5968ab440afbfad134318d16d8bd109ae8b00
fayasPA <fayasmuthalib@gmail.com>
2024-06-23 17:17:31 +0530

    updated vercel.json

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import Layout from './Layout';
 import About from './pages/About/About';
@@ -8,23 +8,29 @@ import SellCar from './pages/SellCar/SellCar';
 import CarDetails from './pages/CarDetail/CarDetails';
 import Gallery from './pages/Gallery/Gallery';
 
+
 function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="vehicles" element={<Cars />}>
-            <Route index element={<h1>Cars Main Page</h1>} />
-            <Route path=":carId/*" element={<CarDetails />} />
-          </Route>
-          <Route path="sellcar" element={<SellCar />} />
-          <Route path="gallery" element={<Gallery />} />
-          <Route path="about" element={<About />} />
+
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<Layout />}>
+        <Route path="" element={<Home />} />
+        <Route path="vehicles" >
+          <Route path="" element={<Cars />} />
+          <Route path=":carId/*" element={<CarDetails />} />
         </Route>
-      </Routes>
-    </Router>
+        <Route path="sellcar" element={<SellCar />} />
+        <Route path="gallery" element={<Gallery />} />
+        <Route path="about" element={<About />} />
+      </Route>
+    )
   );
+  
+  return (
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
+  )
 }
 
-export default App;
+export default App
